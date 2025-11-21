0bf44368d7777cd95ecfceed859566f72eb09de3
julius <julymetodiev@gmail.com>
2025-11-21 22:51:50 +0200

    Update Homebrew formula with v0.1.0 SHA256 checksums

--- homebrew-formula/post-cortex.rb
+++ homebrew-formula/post-cortex.rb
@@ -7,19 +7,19 @@ class PostCortex < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/julymetodiev/post-cortex/releases/download/v#{version}/post-cortex-x86_64-apple-darwin"
-      sha256 "UPDATE_AFTER_RELEASE"
+      sha256 "b4652c9dc7f5501857ab1a8cd4b75bf4b76b0e95682dd3c3174116cdc4622ddd"
 
       resource "daemon" do
         url "https://github.com/julymetodiev/post-cortex/releases/download/v#{version}/post-cortex-daemon-x86_64-apple-darwin"
-        sha256 "UPDATE_AFTER_RELEASE"
+        sha256 "da726a3e5b0fc6f7933f5248241e980f66387c312e29dda83b72052786f46593"
       end
     else
       url "https://github.com/julymetodiev/post-cortex/releases/download/v#{version}/post-cortex-aarch64-apple-darwin"
-      sha256 "UPDATE_AFTER_RELEASE"
+      sha256 "93062e5325c0fdc6232316690fe41d80db9543b3bf9cf48e65d2f95eb531396f"
 
       resource "daemon" do
         url "https://github.com/julymetodiev/post-cortex/releases/download/v#{version}/post-cortex-daemon-aarch64-apple-darwin"
-        sha256 "UPDATE_AFTER_RELEASE"
+        sha256 "0acc00fb276070dae83322a4330761d35bf0bac62531d7138aea088655f80e29"
       end
     end
   end
@@ -27,11 +27,11 @@ class PostCortex < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/julymetodiev/post-cortex/releases/download/v#{version}/post-cortex-x86_64-unknown-linux-gnu"
-      sha256 "UPDATE_AFTER_RELEASE"
+      sha256 "b3ed3e0a56fd836dd96d66c10b1f37026d1aae66f6b77bd8b3dbb6649e235101"
 
       resource "daemon" do
         url "https://github.com/julymetodiev/post-cortex/releases/download/v#{version}/post-cortex-daemon-x86_64-unknown-linux-gnu"
-        sha256 "UPDATE_AFTER_RELEASE"
+        sha256 "db0fa639771640759294d2c57426a81443aa41ac626f34c4ebd52454f1265577"
       end
     end
   end
